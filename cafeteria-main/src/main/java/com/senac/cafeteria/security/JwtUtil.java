bcff43b3d6e335a15d6065deec6335c321f3ca2e
unknown <paulocresp.grispan@gm.gmail.com>
2025-12-01 17:23:40 -0300

    Add a parte wtUtil.java

---
+++ cafeteria-main/src/main/java/com/senac/cafeteria/security/JwtUtil.java
@@ -0,0 +1,74 @@
+package com.senac.cafeteria.security;
+
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+ private final Key key; private final long expirationMillis;
+
+public JwtUtil(@Value("${jwt.secret}") String secret,
+@Value("${jwt.expiration}") long expirationMillis) {
+    this.key = Keys.hmacShaKeyFor(secret.getBytes());
+ this.expirationMillis = expirationMillis;
+}
+
+
+ public String generateToken(String username) {
+
+     Date now = new Date();
+
+      Date expiry = new Date(now.getTime() + expirationMillis);
+
+      return Jwts.builder()
+
+       .setSubject(username)
+
+        .setIssuedAt(now)
+
+         .setExpiration(expiry)
+
+          .signWith(key, SignatureAlgorithm.HS256)
+
+           .compact();
+
+         }
+
+
+          public boolean validateToken(String token) {
+
+            try {
+
+                Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+
+                 return true;
+
+                 } catch (JwtException | IllegalArgumentException ex) {
+
+                     return false;
+
+                    }
+
+                 }
+
+
+                  public String extractUsername(String token) {
+
+                     Claims claims = Jwts.parserBuilder().setSigningKey(key).build()
+
+                      .parseClaimsJws(token).getBody();
+
+                      return claims.getSubject();
+
+                    }
+}
+
+
+
